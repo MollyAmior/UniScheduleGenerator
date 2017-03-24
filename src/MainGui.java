b65742e84e1a1d55bb8c9eaa5684ded91df26493
MollyAmior <mally.amior@gmail.com>
2017-03-24 14:38:27 +0300

    new panel

--- src/MainGui.java
+++ src/MainGui.java
@@ -1,6 +1,12 @@
+import javax.swing.*;
+
 /**
  * Created by malka_000 on 24/03/2017.
  */
 public class MainGui {
-//change
+    private JPanel mainPanel;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
